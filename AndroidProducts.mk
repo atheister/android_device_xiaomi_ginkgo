4a9940f151a663cb39df071b63fefc60972e7ecf
atheister <andraramadhanpr@gmail.com>
2020-12-04 09:26:31 +0000

    ginkgo: Initial DirtyUnicorns bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,8 @@
 #
-# Copyright (C) 2020 ArrowOS
+# Copyright (C) 2020 Dirty Unicorns
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
-
-COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    $(LOCAL_DIR)/du.mk
